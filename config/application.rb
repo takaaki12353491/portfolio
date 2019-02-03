29e20c8fb9d896a3abd19ae99e43ec30855fca9a
takaaki <takaaki12353491@gmail.com>
2019-02-03 20:28:30 +0900

    Create home page

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module Portfolio
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures:       true,
+        view_specs:     false,
+        helper_specs:   true,
+        routing_specs:  true
+    end
   end
 end
